1816f8a9e16e49cc6dfac4f8271c5730897cb0f3
anonymous anonymous <michelle.syu@kellogg.northwestern.edu>
2022-02-24 02:10:59 +0000

    Add forms for new collection objects on show pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,8 @@ class UsersController < ApplicationController
 
   # GET /users/1
   def show
+    @dish = Dish.new
+    @like = Like.new
   end
 
   # GET /users/new
--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -8,6 +8,7 @@ class VenuesController < ApplicationController
 
   # GET /venues/1
   def show
+    @like = Like.new
   end
 
   # GET /venues/new
