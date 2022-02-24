70fe3cc4e0c9d21a492cbe5c9814e465a3ece2e1
anonymous anonymous <michelle.syu@kellogg.northwestern.edu>
2022-02-24 02:12:14 +0000

    Remove controller comments

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,27 +1,22 @@
 class VenuesController < ApplicationController
   before_action :set_venue, only: %i[show edit update destroy]
 
-  # GET /venues
   def index
     @q = Venue.ransack(params[:q])
     @venues = @q.result(distinct: true).includes(:likes,
                                                  :favorite_dishes).page(params[:page]).per(10)
   end
 
-  # GET /venues/1
   def show
     @like = Like.new
   end
 
-  # GET /venues/new
   def new
     @venue = Venue.new
   end
 
-  # GET /venues/1/edit
   def edit; end
 
-  # POST /venues
   def create
     @venue = Venue.new(venue_params)
 
@@ -32,7 +27,6 @@ class VenuesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /venues/1
   def update
     if @venue.update(venue_params)
       redirect_to @venue, notice: "Venue was successfully updated."
@@ -41,7 +35,6 @@ class VenuesController < ApplicationController
     end
   end
 
-  # DELETE /venues/1
   def destroy
     @venue.destroy
     redirect_to venues_url, notice: "Venue was successfully destroyed."
@@ -49,12 +42,10 @@ class VenuesController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_venue
     @venue = Venue.find(params[:id])
   end
 
-  # Only allow a trusted parameter "white list" through.
   def venue_params
     params.require(:venue).permit(:name, :address, :neighborhood)
   end
